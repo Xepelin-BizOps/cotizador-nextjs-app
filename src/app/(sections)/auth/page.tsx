634cc85dc2c9be7d2d04bea0e37ee8c1e88b32a5
joaquin <joaqui222@gmail.com>
2025-08-11 16:35:02 -0300

    feat: now receive the postMessage

--- src/app/(sections)/auth/page.tsx
+++ src/app/(sections)/auth/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+import { Alert, Spin } from "antd";
+import useLogin from "./_fetures/hooks/useLogin";
+
 export default function page() {
+  // Login - PostMessage
+  const { data } = useLogin();
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-center text-accent">
-        Login page: Validar sessión
-      </h1>
+    <div className="flex-1 flex flex-col items-center">
+      <h2 className="m-4 text-2xl text-accent font-bold">Validar la sesión</h2>
+
+      {data.isLoading && (
+        <Spin tip="Validando sesión..." size="large" fullscreen />
+      )}
+
+      {data?.error && (
+        <Alert
+          showIcon
+          type="error"
+          message={"Error"}
+          description={data.message}
+        />
+      )}
     </div>
   );
 }
